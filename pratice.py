22705daad430e27ac3b2bd54db2a85131bd84e21
nikkkkieeeee <87739897+nikkkkieeeee@users.noreply.github.com>
2021-08-08 16:13:09 +0800

    dddd

--- pratice.py
+++ pratice.py
@@ -2,6 +2,10 @@ import pandas as pd
 data = pd.read_excel('dataNew.xls')
 print(data)
 
+data.index = data['Period']
+del data['Period']
+data.index
+
 data.head(11)
 data.loc[10:30]
 data.tail(11)
@@ -10,9 +14,12 @@ print(data.loc[11:19])
 print(data.tail(11))
 
 import matplotlib.pyplot as plt
-plt.bar()
-plt.title('1900-1910')
-plt.xlabel('Year')
-plt.ylabel('No. of calories')
-plt.show()
-hellohello
+ax = data[['Calories', 'Period']].plot(kind='bar', title="1900 to 1910", figsize=(10,10), legend=True, fontsize=12)
+plt.show();
+
+# import matplotlib.pyplot as plt
+# plt.bar()
+# plt.title('1900-1910')
+# plt.xlabel('Year')
+# plt.ylabel('No. of calories')
+# plt.show();
