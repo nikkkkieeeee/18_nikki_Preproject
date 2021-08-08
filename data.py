52cf2a05fb28b27bd910ad4b8b491d1f5eb91e24
cecilia2002 <84916123+cecilia2002@users.noreply.github.com>
2021-08-08 17:20:30 +0800

    hii

--- data.py
+++ data.py
@@ -1,17 +1,62 @@
-import pandas as pd
-data = pd.read_excel('dataNew.xls')
-print(data)
-
-data.head(11)
-data.loc[10:30]
-data.tail(11)
-print(data.head(11))
-print(data.loc[11:19])
-print(data.tail(11))
-
 import matplotlib.pyplot as plt
-plt.bar()
-plt.title('1900-1910')
-plt.xlabel('Year')
-plt.ylabel('No. of calories')
+import pandas as pd
+import numpy as np
+file = pd.read_excel('dataNew.xls')
+print(file)
+
+date = file['Period'].str.split(' ', n = 1, expand = True)
+print(date)
+
+file = file.assign(date_year=date[1])
+print(file)
+
+file.index = file['date_year']
+print(file.index)
+
+grp1 = file.iloc[:11]
+print(grp1)
+grp2 = file.iloc[11:21]
+print(grp2)
+grp3 = file.iloc[21:]
+print(grp3)
+
+grp1sum = sum(grp1['Calories'])
+print(round(grp1sum, 1))
+grp2sum = sum(grp2['Calories'])
+print(grp2sum)
+grp3sum = sum(grp3['Calories'])
+print(grp3sum)
+
+grp1mean = grp1sum / len(grp1['Calories'])
+print(round(grp1mean, 2))
+grp2mean = grp2sum / len(grp2['Calories'])
+print(round(grp2mean, 2))
+grp3mean = grp3sum / len(grp3['Calories'])
+print(round(grp3mean, 2))
+
+ps = grp1['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.xlabel('Year', fontsize=10)
+plt.ylabel('No. of calories', fontsize=10)
+plt.xticks(index, ps.index, fontsize=10)
+plt.title('[1900-1910]')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+ps = grp2['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.xlabel('Year', fontsize=10)
+plt.ylabel('No. of calories', fontsize=10)
+plt.xticks(index, ps.index, fontsize=10)
+plt.title('[1911-1920]')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+ps = grp3['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.xlabel('Year', fontsize=10)
+plt.ylabel('No. of calories', fontsize=10)
+plt.xticks(index, ps.index, fontsize=10)
+plt.title('[1921-1930]')
+plt.bar(ps.index, ps.values)
 plt.show()
--- pratice.py
+++ pratice.py
@@ -2,29 +2,32 @@ import pandas as pd
 data = pd.read_excel('dataNew.xls')
 print(data)
 
-data.index = data['Period']
-del data['Period']
-data.index
+data = data['Period'].str.split(' ', n = 1, expand = True)
+print()
 
-grp1 = data.head(11)
-print(grp1)
+set1 = set.assign(grp1date_year=set1date[1])
+print(set1)
 
-# data.iloc[11:20]
-# print(data.iloc[11:20])
-#
-# data.tail(11)
-# print(data.tail(11))
+set1.index = grp1['Period']
+print(set1.index)
+
+set1 = data.head(11)
+print(data.head(11))
+
+data.iloc[11:20]
+print(data.iloc[11:20])
+
+data.tail(11)
+print(data.tail(11))
 
 import numpy as np
 import matplotlib.pyplot as plt
-data = grp1['Calories'].plot(kind='bar', title="1900-1910", figsize=(10,10), legend=True, fontsize=10)
-plt.show();
-# ps = grp1['Calories'].sorted_values()
-# index = np.arange(len(ps.index))
-# plt.bar(ps.index, ps.values)
-# plt.xlabel("Year", fontsize=15)
-# plt.ylabel("No. of calories", fontsize=15)
-# plt.xticks(index, ps.index, fontsize=15)
-# plt.title("[1900-1910]", size=18)
-# plt.show()
+ps = data['Calories'].sorted_values()
+index = np.arange(len(ps.index))
+plt.bar(ps.index, ps.values)
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("No. of calories", fontsize=15)
+plt.xticks(index, ps.index, fontsize=15)
+plt.title("[1900-1910]", size=18)
+plt.show()
 
