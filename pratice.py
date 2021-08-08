030324819eb2eaef6689e0823cd8f73cc645451a
nikkkkieeeee <87739897+nikkkkieeeee@users.noreply.github.com>
2021-08-08 17:19:35 +0800

    dddd

--- pratice.py
+++ pratice.py
@@ -2,29 +2,51 @@ import pandas as pd
 data = pd.read_excel('dataNew.xls')
 print(data)
 
-data.index = data['Period']
-del data['Period']
-data.index
+datas = data['Period'].str.split(' ', n = 1, expand = True)
+print(datas)
 
-grp1 = data.head(11)
-print(grp1)
+data = data.assign(year=datas[1])
 
-# data.iloc[11:20]
-# print(data.iloc[11:20])
-#
-# data.tail(11)
-# print(data.tail(11))
+data.index = data['year']
+del data['year']
+print(data.index)
+
+set1 = data.head(11)
+print(set1)
+
+set2 = data.iloc[11:21]
+print(set2)
+
+set3 = data.tail(10)
+print(set3)
 
 import numpy as np
 import matplotlib.pyplot as plt
-data = grp1['Calories'].plot(kind='bar', title="1900-1910", figsize=(10,10), legend=True, fontsize=10)
+
+ps = set1['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.bar(ps.index, ps.values)
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("No. of calories", fontsize=15)
+plt.xticks(index, ps.index, fontsize=15)
+plt.title("[1900-1910]", size=18)
 plt.show();
-# ps = grp1['Calories'].sorted_values()
-# index = np.arange(len(ps.index))
-# plt.bar(ps.index, ps.values)
-# plt.xlabel("Year", fontsize=15)
-# plt.ylabel("No. of calories", fontsize=15)
-# plt.xticks(index, ps.index, fontsize=15)
-# plt.title("[1900-1910]", size=18)
-# plt.show()
+
+ps = set2['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.bar(ps.index, ps.values)
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("No. of calories", fontsize=15)
+plt.xticks(index, ps.index, fontsize=15)
+plt.title("[1911 to 1920]", size=18)
+plt.show();
+
+ps = set3['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.bar(ps.index, ps.values)
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("No. of calories", fontsize=15)
+plt.xticks(index, ps.index, fontsize=15)
+plt.title("[1921 to 1930]", size=18)
+plt.show();
 
